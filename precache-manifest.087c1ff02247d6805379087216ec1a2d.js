26763fec54eced8e54198bffafa9debf65ed0833
Darío Vallés <dario@erikalustfilms.com>
2020-03-30 09:53:37 +0200

    Initial dist subtree commit

---
+++ precache-manifest.087c1ff02247d6805379087216ec1a2d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb63ca15bba16b550232b0b0f66c726",
+    "url": "/c19-india/OneSignalSDKUpdaterWorker.js"
+  },
+  {
+    "revision": "ebb63ca15bba16b550232b0b0f66c726",
+    "url": "/c19-india/OneSignalSDKWorker.js"
+  },
+  {
+    "revision": "9169b8be8c32a5f0d1fa",
+    "url": "/c19-india/css/app.b72bf63d.css"
+  },
+  {
+    "revision": "032e855a26a1397577c73a1473c7ca93",
+    "url": "/c19-india/index.html"
+  },
+  {
+    "revision": "af98dadb561332abfd74",
+    "url": "/c19-india/js/about.6555691f.js"
+  },
+  {
+    "revision": "9169b8be8c32a5f0d1fa",
+    "url": "/c19-india/js/app.4339fef2.js"
+  },
+  {
+    "revision": "ff8ce75dd0baae763004",
+    "url": "/c19-india/js/chunk-vendors.af2c007a.js"
+  },
+  {
+    "revision": "8abf7185591fdf2e991359c7373c9efa",
+    "url": "/c19-india/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/c19-india/robots.txt"
+  }
+]);
